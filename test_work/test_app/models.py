7a285b619f17a129ae3255ccaeb777c992082899
Артем <artemmokrecov@yandex.ru>
2020-08-24 11:32:51 +0300

    add bootstrap to /test page

--- test_work/test_app/models.py
+++ test_work/test_app/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.core import exceptions
+import json
+import requests
 
 # Create your models here.
 class Users(models.Model):
@@ -30,6 +33,64 @@ class Posts(models.Model):
     username = models.CharField(max_length=250)
     title = models.CharField(max_length=250)
     body = models.CharField(max_length=250)
-    def __srt__(self):
-        data = [self.post_id, self.user_id, self.title, self.body]
-        return data
+    def get_data(self):
+        all_posts = []
+            #Запрос данных
+        users = json.loads(requests.get('http://jsonplaceholder.typicode.com/users').content)
+        posts = json.loads(requests.get('http://jsonplaceholder.typicode.com/posts').content)
+        
+        #Заполняем базу данными юзеров
+        for user in users:
+            try:
+                user_temp = Users.objects.get(user_id=user['id'])
+            except exceptions.ObjectDoesNotExist:
+                user_data = Users(user_id=user['id'],
+                                    name=user['name'],
+                                    username=user['username'],
+                                    email=user['email'],
+                                    phone=user['phone'],
+                                    website=user['website'],
+                                    )
+                user_data.save()
+
+                adress_data = Adress(user_id=Users(user_id=user['id']),
+                                    city=user['address']['city'],
+                                    street=user['address']['street'],
+                                    suite=user['address']['suite'],
+                                    zipcode=user['address']['zipcode'],
+                                    lat = user['address']['geo']['lat'],
+                                    lng = user['address']['geo']['lng'],
+                                    )
+                adress_data.save()
+
+                company_data = Company(user_id=Users(user['id']),
+                                        name=user['company']['name'],
+                                        catchPhrase=user['company']['catchPhrase'],
+                                        bs=user['company']['bs'],
+                                        )
+                company_data.save()
+                
+        #Заполняем базу данными постов
+        for post in posts:
+            try:
+                post_temp = Posts.objects.get(post_id=post['id'])
+            except exceptions.ObjectDoesNotExist: 
+                user = Users.objects.get(user_id=post['userId'])
+                posts_data = Posts(user_id=Users(post['userId']),
+                                    post_id=post['id'],
+                                    title=post['title'],
+                                    body=post['body'],
+                                    username=user.username,
+                                    )
+                posts_data.save()
+
+        #Вывод данных на страницу
+        
+        for post in Posts.objects.all():
+            data = {
+                'name': post.username,
+                'title': post.title,
+                'body':post.body,
+            }
+            all_posts.append(data)
+        return all_posts
--- test_work/test_app/views.py
+++ test_work/test_app/views.py
@@ -4,67 +4,12 @@ from django.shortcuts import render
 from .models import Posts, Users, Adress, Company
 from django.core import exceptions
 
-
 # Create your views here.
+
 def index(request):
-    #Запрос данных
-    users = json.loads(requests.get('http://jsonplaceholder.typicode.com/users').content)
-    posts = json.loads(requests.get('http://jsonplaceholder.typicode.com/posts').content)
-    
-    #Заполняем базу данными юзеров
-    for user in users:
-        try:
-            user_temp = Users.objects.get(user_id=user['id'])
-        except exceptions.ObjectDoesNotExist:
-            user_data = Users(user_id=user['id'],
-                                name=user['name'],
-                                username=user['username'],
-                                email=user['email'],
-                                phone=user['phone'],
-                                website=user['website'],
-                                )
-            user_data.save()
-
-            adress_data = Adress(user_id=Users(user_id=user['id']),
-                                city=user['address']['city'],
-                                street=user['address']['street'],
-                                suite=user['address']['suite'],
-                                zipcode=user['address']['zipcode'],
-                                lat = user['address']['geo']['lat'],
-                                lng = user['address']['geo']['lng'],
-                                )
-            adress_data.save()
-
-            company_data = Company(user_id=Users(user['id']),
-                                    name=user['company']['name'],
-                                    catchPhrase=user['company']['catchPhrase'],
-                                    bs=user['company']['bs'],
-                                    )
-            company_data.save()
-            
-    #Заполняем базу данными постов
-    for post in posts:
-        try:
-            post_temp = Posts.objects.get(post_id=post['id'])
-        except exceptions.ObjectDoesNotExist: 
-            user = Users.objects.get(user_id=post['userId'])
-            posts_data = Posts(user_id=Users(post['userId']),
-                                post_id=post['id'],
-                                title=post['title'],
-                                body=post['body'],
-                                username=user.username,
-                                )
-            posts_data.save()
-
-    #Вывод данных на страницу
-    all_posts = []
-    for post in Posts.objects.all():
-        data = {
-            'name': post.username,
-            'title': post.title,
-            'body':post.body,
-        }
-        all_posts.append(data)
-    
-    context = {'info':all_posts}
+    context = {'info':Posts.get_data(self=Posts)}
     return(render(request, 'index.html', context))
+
+def test(request):
+    context = {'info':Posts.get_data(self=Posts)}
+    return(render(request, 'test.html', context))
